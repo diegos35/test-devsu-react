f87b7ecd17bb4eaf82fff10635f8d3686d95b0ac
Diego-caviedes <diego_1996.c.c@hotmail.com>
2023-06-13 02:10:34 -0500

    add styles to table

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import ProductTable from '../../components/Table'
 
 function Home() {
+  const products = [
+    {
+      "id": 1,
+      "logo": "logo1.jpg",
+      "nombre": "Producto 1",
+      "descripcion": "Descripción del Producto 1",
+      "fechaLiberacion": "2023-01-01",
+      "fechaRestructuracion": "2023-02-01"
+    },
+    {
+      "id": 2,
+      "logo": "logo2.jpg",
+      "nombre": "Producto 2",
+      "descripcion": "Descripción del Producto 2",
+      "fechaLiberacion": "2023-03-01",
+      "fechaRestructuracion": "2023-04-01"
+    },
+    {
+      "id": 3,
+      "logo": "logo3.jpg",
+      "nombre": "Producto 3",
+      "descripcion": "Descripción del Producto 3",
+      "fechaLiberacion": "2023-05-01",
+      "fechaRestructuracion": "2023-06-01"
+    }
+  ];
+
   return (
     <div>
-      Home
+      <ProductTable products= {products} />
     </div>
   )
 }
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../styles/Table.css';
 
 const ProductTable = ({ products }) => {
+  const [activeMenu, setActiveMenu] = useState(null);
+
+  const handleEdit = (productId) => {
+    // Lógica para editar el producto con el ID proporcionado
+    console.log(`Editar producto con ID: ${productId}`);
+  };
+
+  const handleDelete = (productId) => {
+    // Lógica para eliminar el producto con el ID proporcionado
+    console.log(`Eliminar producto con ID: ${productId}`);
+  };
+
+  const toggleMenu = (productId) => {
+    console.log( activeMenu, productId)
+    if (activeMenu === productId) {
+      setActiveMenu(null);
+    } else {
+      setActiveMenu(productId);
+    }
+  };
+
   return (
-    <table>
+    <div>
+      <table>
       <thead>
-        <tr>
+        <tr colSpan="5">
           <th>Logo</th>
           <th>Nombre del producto</th>
           <th>Descripción</th>
           <th>Fecha de liberación</th>
           <th>Fecha de reestructuración</th>
+          <th class="menu-column"></th>
         </tr>
       </thead>
       <tbody>
@@ -20,12 +44,36 @@ const ProductTable = ({ products }) => {
             <td>{product.descripcion}</td>
             <td>{product.fechaLiberacion}</td>
             <td>{product.fechaRestructuracion}</td>
+            <td>
+              <div className="menu" onClick={() => toggleMenu(product.id)}>
+                <span className="icon">...</span>
+                {activeMenu === product.id && (
+                  <div className="menu-content">
+                    <div 
+                      className="menu-item" 
+                      onClick={() => handleEdit(product.id)}
+                    >
+                        Editar
+                    </div>
+                    <div 
+                      className="menu-item" 
+                      onClick={() => handleDelete(product.id)}
+                    >
+                        Eliminar
+                    </div>
+                  </div>
+                )}
+              </div>
+            </td>
           </tr>
         ))}
       </tbody>
-    </table>
+      </table>
+      <p className='total-elements'> {products.length} Resultados</p>
+    </div>
   );
 };
 
 
-export default App;
+
+export default ProductTable;
