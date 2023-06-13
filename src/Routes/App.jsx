104fb73a0074d13525892901599ae04cd5bf75ba
Diego-caviedes <diego_1996.c.c@hotmail.com>
2023-06-13 01:01:29 -0500

    implement routes with react-route

--- src/Routes/App.jsx
+++ src/Routes/App.jsx
@@ -1,13 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import '../App.css'
 import Home from '../Pages/Home/Home'
+import Form from '../Pages/Form/Form'
+import NotFound from '../Pages/NotFound/NotFound'
 
-function App() {
-//  const [count, setCount] = useState(0)
-
+const App = () => {
+ 
   return (
-    <>
-    <Home />
-    </>
+    <BrowserRouter>
+     <Routes>
+        <Route exact path='/' element={ <Home /> }/>
+        <Route exact path="/form" element={ <Form /> } />
+        <Route path="*" element={ < NotFound />} /> 
+        </Routes>
+    </BrowserRouter>
   )
 }
 
